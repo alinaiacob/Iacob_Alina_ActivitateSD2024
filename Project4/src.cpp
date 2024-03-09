513de1103204d92c9afdb6641d8e2ba6260962c6
Alina Iacob <alinavalentinaiacob@gmail.com>
2024-03-09 15:18:19 +0200

    Add project files.

--- Project4/src.cpp
+++ Project4/src.cpp
@@ -7,6 +7,7 @@
 #include <string.h>
 
 
+#define MAX_LINE_LENGTH 100
 
 struct Preparat {
 
@@ -21,6 +22,73 @@ struct Preparat {
 };
 
 
+
+struct Preparat citestePreparatDinFisier(const char* numeFisier) {
+    FILE* f = fopen(numeFisier, "r");
+    struct Preparat p;
+    char line[MAX_LINE_LENGTH];
+    if (f != NULL) {
+        while (fgets(line, MAX_LINE_LENGTH, f)) {
+            char* token = strtok(line, ",\n");
+            // Alocare memorie pentru numele preparatului și copierea acestuia
+            p.numePreparat = (char*)malloc(strlen(token) + 1);
+            strcpy(p.numePreparat, token);
+
+            token = strtok(NULL, ",\n");
+            p.nrProduseNecesare = atoi(token);
+
+            // Alocare memorie pentru array-ul de pointeri către numele produselor
+            p.numeleProduselorNecesare =(char**) malloc(p.nrProduseNecesare * sizeof(char*));
+
+            int contor = 0;
+            while (contor < p.nrProduseNecesare) {
+                token = strtok(NULL, ",\n");
+                // Alocare memorie pentru fiecare nume de produs și copierea acestuia
+                p.numeleProduselorNecesare[contor] = (char*)malloc(strlen(token) + 1);
+                strcpy(p.numeleProduselorNecesare[contor], token);
+                contor++;
+            }
+
+            token = strtok(NULL, ",\n");
+            p.timpPregatire = atoi(token);
+        }
+        fclose(f);
+    }
+    return p;
+}
+struct Preparat* citestePreparateDinFisier(const char* numeFisier, int* nrPreparate) {
+    FILE* f = fopen(numeFisier, "r");
+    if (f != NULL) {
+        char line[MAX_LINE_LENGTH];
+        int nrLinii = 0;
+        while (fgets(line, MAX_LINE_LENGTH, f))nrLinii++;
+        fseek(f, 0, SEEK_SET);
+        struct Preparat* preparate =(struct Preparat*)malloc(nrLinii * sizeof(struct Preparat));
+        *nrPreparate = 0;
+        while (fgets(line, MAX_LINE_LENGTH, f)) {
+            char *token = strtok(line, ",\n");
+            preparate[*nrPreparate].numePreparat = (char*)malloc(strlen(token) + 1);
+            strcpy(preparate[*nrPreparate].numePreparat, token);
+
+            token = strtok(NULL, ",\n");
+            preparate[*nrPreparate].nrProduseNecesare = atoi(token);
+            preparate[*nrPreparate].numeleProduselorNecesare = (char**)malloc(preparate[*nrPreparate].nrProduseNecesare * sizeof(char*));
+            int contor = 0;
+            while (contor < preparate[*nrPreparate].nrProduseNecesare) {
+                token = strtok(NULL, ",\n");
+                preparate[*nrPreparate].numeleProduselorNecesare[contor] = (char*)malloc(strlen(token) + 1);
+                strcpy(preparate[*nrPreparate].numeleProduselorNecesare[contor], token);
+                contor++;
+            }
+            token = strtok(NULL, ",\n");
+            preparate[*nrPreparate].timpPregatire = atoi(token);
+            (*nrPreparate)++;
+        }
+        fclose(f);
+        return preparate;
+    }
+    return NULL;
+}
 
 
 void citestePreparat(struct Preparat* p) {
@@ -138,35 +206,83 @@ void afiseazaAnumitePreparate(struct Preparat preparate[], struct Preparat* anum
     }
 
 }
+static void scriePreparatInFisier(struct Preparat preparat,const char *numeFisier) {
+    FILE* f = fopen(numeFisier, "w");
+    if (f != NULL) {
+        if (preparat.numePreparat != NULL) {
+            fprintf(f, "%s,", preparat.numePreparat);
+        }
+        else {
+            fprintf(f, " ,");
+        }
+        fprintf(f, "%d,", preparat.nrProduseNecesare);
+        for (int i = 0; i < preparat.nrProduseNecesare; i++) {
+            fprintf(f, "%s,", preparat.numeleProduselorNecesare[i]);
+        }
+        fprintf(f, "%d", preparat.timpPregatire);
+        fclose(f);
+    }
+    else printf("\Nu s-a putut deschide fisierul :((");
+}
+static void scriePreparateInFisier(struct Preparat* preparate,int nrPreparate, const char* numeFisier) {
+    FILE* f = fopen(numeFisier, "w");
+    if (f != NULL) {
+        if (preparate != NULL && nrPreparate>0) {
+            for (int i = 0; i < nrPreparate; i++) {
+                if (preparate[i].numePreparat != NULL) {
+                    fprintf(f, "%s,", preparate[i].numePreparat);
+                }
+                else {
+                    fprintf(f, " ,");
+                }
+                fprintf(f, "%d,", preparate[i].nrProduseNecesare);
+                for (int i = 0; i < preparate[i].nrProduseNecesare; i++) {
+                    fprintf(f, "%s,", preparate[i].numeleProduselorNecesare[i]);
+                }
+                fprintf(f, "%d\n", preparate[i].timpPregatire);
+            }
+            fclose(f);
+        }
+        else printf("\nNu avem ce sa scriem in fisier:((");
+    }
+}
+//static void scriePrepa
 
 int main() {
 
     struct Preparat preparate[5];
 
-    for (int i = 0; i < 5; i++) {
+    //for (int i = 0; i < 5; i++) {
 
-        citestePreparat(&preparate[i]);
+    //    citestePreparat(&preparate[i]);
 
-    }
-
-
-
-    for (int i = 0; i < 5; i++) {
-
-        free(preparate[i].numePreparat);
-
-        for (int j = 0; j < preparate[i].nrProduseNecesare; j++) {
-
-            free(preparate[i].numeleProduselorNecesare[j]);
-
-        }
-
-        free(preparate[i].numeleProduselorNecesare);
-
-    }
+    //}
 
 
 
+    //for (int i = 0; i < 5; i++) {
+
+    //    free(preparate[i].numePreparat);
+
+    //    for (int j = 0; j < preparate[i].nrProduseNecesare; j++) {
+
+    //        free(preparate[i].numeleProduselorNecesare[j]);
+
+    //    }
+
+    //    free(preparate[i].numeleProduselorNecesare);
+
+    //}
+   // struct Preparat p=citestePreparatDinFisier("Preparate.txt");
+    int nrPreparate = 0;
+    struct Preparat* p = citestePreparateDinFisier("Preparate.txt", &nrPreparate);
+    printf("%d", nrPreparate);
+    afiseazaPreparatul(p[1]);
+   // FILE *f=fopen("Test.txt")
+    FILE* f; f = fopen("Test.txt", "w");
+   // scriePreparatInFisier2(p[0], f);
+    scriePreparatInFisier(p[1], "Test.txt");
+    scriePreparateInFisier(p, 2, "Teste.txt");
     return 0;
 
 }
