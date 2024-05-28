44c26949698e4f7440719d3b0af003c725be3b20
alinaiacob <84909271+alinaiacob@users.noreply.github.com>
2024-05-28 15:11:23 +0300

    Add files via upload

--- arbori.c
+++ arbori.c
@@ -1,120 +1,99 @@
-#define _CRT_SECURE_NO_WARNINGS	
 #include<stdio.h>
 #include<stdlib.h>
 #include<string.h>
-typedef struct Cinema Cinema;
-typedef struct Nod Nod;
-struct Cinema {
-	int id;
-	int nrFilme;
-	char** numeFilme;
+
+typedef struct Camion Camion;
+typedef struct NodSecundar NodSecundar;
+typedef struct NodPrincipal NodPrincipal;
+
+struct Camion {
+	int serie;
+	char* marca;
+
+	float greutate;
+
 };
-struct Nod {
-	Cinema info;
-	Nod* st;
-	Nod* dr;
+struct NodSecundar {
+	NodPrincipal* nod;
+	NodSecundar* next;
+};
+struct NodPrincipal {
+	Camion info;
+	NodSecundar* vecini;
+	NodPrincipal* next;
 };
 
-Cinema initCinema() {
-
-}
-void inserareInArbore(Nod** radacina, Cinema c) {
-	if (*radacina != NULL) {
-		if ((*radacina)->info.id > c.id) {
-			inserareInArbore(&((*radacina)->st), c);
-		}
-		else {
-			inserareInArbore(&((*radacina)->dr), c);
-		}
-	}
-	else {
-		Nod* nod = (Nod*)malloc(sizeof(Nod));
-		nod->info = c;
-		nod->st = NULL;
-		nod->dr = NULL;
-		*radacina = nod;
-	}
+//inserare in lista principala la inceput
+NodPrincipal* inserarePrincipala(NodPrincipal* graf, Camion c) {
+	NodPrincipal* nou = (NodPrincipal*)malloc(sizeof(NodPrincipal));
+	nou->info = c;
+	nou->next = graf;
+	nou->vecini = NULL;
+	return nou;
 }
 
-Cinema citireCinema(FILE *f) {
-	Cinema cinema;
-	fscanf(f, "%d", cinema.id);
-	fscanf(f, "%d", cinema.nrFilme);
-	if (cinema.nrFilme > 0) {
-		cinema.numeFilme = (char**)malloc(sizeof(char*) * cinema.nrFilme);
+//functie de cautare NodPricipal dupa Serie camion
 
-		for (int i = 0; i < cinema.nrFilme; i++) {
-			char buffer[100];
-			fscanf(f, "%s", &buffer);
-			cinema.numeFilme[i] = (char*)malloc((strlen(buffer) + 1) * sizeof(char));
-			strcpy(cinema.numeFilme[i], buffer);
-
-		}
-		
-	}
-	else {
-		cinema.id = -1;
-		cinema.nrFilme = 0;
-		cinema.numeFilme = NULL;
-	}
-	return cinema;
+ NodPrincipal *cautaNodDupaSerie(NodPrincipal *graf,int serie) {
+	 while (graf && graf->info.serie != serie) {
+		 graf = graf->next;
+	 }
+	 return graf;
 }
 
-Nod* citireFisier(const char* numeFisier) {
-	Nod* radacina = NULL;
-	if (numeFisier && strlen(numeFisier)>0) {
-		FILE* f = fopen(numeFisier, "r");
-		if (f) {
-			int dim;
-			fscanf(f, "%d",&dim);
-		
-			for (int i = 0; i < dim; i++) {
-				Cinema cinema=citireCinema(f);
-				inserareInArbore(&radacina, cinema);
-			}
-          //  printf("%d", dim);
-			fclose(f);
-		}
-		else {
 
-		}
-    }
-	return radacina;
-}
-void afisareCinema(Cinema c) {
-	printf("Cinematograful cu id-ul %d are in derulare %d filme", c.id, c.nrFilme);
-	for (int i = 0; i < c.nrFilme; i++) {
-		printf("%s, ", c.numeFilme[i]);
-	}
-}
-void afisareArborePreOrdine(Nod* rad) {
-	if (rad) {
-		afisareCinema(rad->info);
-		afisareArborePreOrdine(rad->st);
-		afisareArborePreOrdine(rad->dr);
-	}
-}
-void afisareArboreInOrdine(Nod* rad) {
-	if (rad) {
-		afisareArboreInOrdine(rad->st);
-		afisareCinema(rad->info);
-		afisareArboreInOrdine(rad->dr);
-	}
-}
-int calculNrFilmeRedate(Nod* radacina) {
-	if (radacina) {
-		int suma = radacina->info.nrFilme;
-		suma += calculNrFilmeRedate(radacina->st);
-		suma += calculNrFilmeRedate(radacina->dr);
-		return suma;
-	}
-	else {
-		return 0;
-	}
-}
+
+
+
+ //inserare la sfarsit pentru lista secundara
+ void inserareSecundara(NodSecundar **cap,NodPrincipal *nod) {
+	 NodSecundar* nou = (NodSecundar*)malloc(sizeof(NodSecundar));
+	 nou->nod = nod;
+	 nou->next = NULL;
+	 if (*cap) {
+		 NodSecundar* p = *cap;
+		 while (p->next) {
+			 p = p->next;
+
+		 }
+		 p->next = nou;
+	 }
+	 else {
+		 *cap = nou;
+	 }
+ }
+
+//inserare muchie in lista
+ void inserareMuchie(NodPrincipal *graf,int serieStart,int serieStop) {
+	 NodPrincipal* nodStart = cautaNodDupaSerie(graf, serieStart);
+	 NodPrincipal* nodStop = cautaNodDupaSerie(graf, serieStop);
+	 inserareSecundara(&(nodStart->vecini), nodStop);
+	 inserareSecundara(&(nodStop->vecini), nodStart);
+
+ }
+ Camion creareCamion(int serie, const char* marca, float greutate) {
+	 Camion c;
+	 c.serie = serie;
+	 c.greutate = greutate;
+	 c.marca = (char*)malloc(sizeof(char) * (strlen(marca) + 1));
+	 return c;
+ }
+
+
 void main() {
-	Nod* arbore = NULL;
-	arbore=citireFisier("cinema.txt");
-	afisareArborePreOrdine(arbore);
-	
+
+	NodPrincipal* graf = NULL;
+	//inseram 5 noduri cu id-urile 0 1 2 3 4
+	graf = inserarePrincipala(graf, creareCamion(4, "Scania", 20));
+	graf = inserarePrincipala(graf, creareCamion(3, "Volvo", 30));
+	graf = inserarePrincipala(graf, creareCamion(2, "Renault", 25));
+	graf = inserarePrincipala(graf, creareCamion(1, "MAN", 15));
+	graf = inserarePrincipala(graf, creareCamion(0, "Mercedes", 40));
+	inserareMuchie(graf, 0, 1);
+	inserareMuchie(graf, 1, 2);
+	inserareMuchie(graf, 0, 3);
+	inserareMuchie(graf, 2, 4);
+	inserareMuchie(graf, 3, 4);
+	inserareMuchie(graf, 1, 3);
+	//inserareMuchie(graf, 0, 1);
 }
